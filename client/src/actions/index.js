12401b9648c535d8298b17095fd7e091ae633ecb
James <james.lee4789@gmail.com>
2019-06-30 17:52:41 -0400

    add all actions for project

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,18 +1,67 @@
 import streams from '../api/streams'
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM
+} from 'types';
 
 export const signIn = (userId) => {
   return {
-    type: 'SIGN_IN',
+    type: SIGN_IN,
     payload: userId
   };
 };
 
 export const signOut = () => {
   return {
-    type: 'SIGN_OUT'
+    type: SIGN_OUT
   };
 };
 
 export const createStream = formValues => async dispatch => {
-  streams.post('/streams', formValues);
+  const res = await streams.post('/streams', formValues);
+
+  dispatch({
+    type: CREATE_STREAM,
+    payload: res.data
+  })
+}
+
+export const fetchStreams = () => async dispatch => {
+  const res = await streams.get('/streams');
+
+  dispatch({
+    type: FETCH_STREAMS,
+    payload: res.data
+  })
+}
+
+export const fetchStream = (id) => async dispatch => {
+  const res = await streams.get(`/streams/${id}`);
+
+  dispatch({
+    type: FETCH_STREAM,
+    payload: res.data
+  })
+}
+
+export const deleteStream = (id) => async dispatch => {
+  const res = await streams.delete(`/streams/${id}`);
+
+  dispatch({
+    type: DELETE_STREAM
+  })
+}
+
+export const editStream = (id) => async dispatch => {
+  const res = await streams.put(`/streams/${id}`);
+
+  dispatch({
+    type: EDIT_STREAM,
+    payload: res.data
+  })
 }
