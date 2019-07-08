644ac0ceb7f0b3eba9c80b1ace28ec68359d4ce8
James <james.lee4789@gmail.com>
2019-07-07 20:28:05 -0400

    add edit changes with itiail value

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {fetchStream} from '../../actions';
+import {fetchStream, editStream} from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
+
+  onSubmit = (formValues) =>  {
+    this.props.editStream(formValues);
+  }
+
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>
     }
-    return <div>this.props.stream.title</div>;
+    return(
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm initialValues={this.props.stream} onSubmit={this.onSubmit} />
+      </div>
+    )
   }
+}
 
 const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id]};
 }
 
-export default connect(mapStateToProps)(StreamEdit);
+export default connect(mapStateToProps, {fetchStream,editStream})(StreamEdit);
